c7fdf86d7353abda7985c3e76f0ee6ce3298bfa5
jnillo <juan.vazquez@betybyte.com>
2011-03-21 20:40:33 +0100

    Solucionado el puñetero problema de la migrate y construccion completa del modelo PicUps

--- app/controllers/pic_ups_controller.rb
+++ app/controllers/pic_ups_controller.rb
@@ -3,7 +3,8 @@ class PicUpsController < ApplicationController
   def index
     debugger
 
-    @project = PicUps.find params[:project_id] || create_pic_to_project(params[:project_id])
+    @project_picups = PicUp.find_by_id params[:project_id] || create_pic_to_project(params[:project_id])
+    @project = Project.find_by_identifier params[:project_id]
   end
 
   def my_pics
--- db/migrate/20110317190252_create_pic_ups.rb
+++ db/migrate/20110317190252_create_pic_ups.rb
@@ -1,7 +1,8 @@
 class CreatePicUps < ActiveRecord::Migration
+  #NOTE: Borrar la tabla PluginSchemaInfo el registro de la migrate para poder lanzarla de nuevo
   def self.up
-    create_table :pic_ups do |t|
-      t.column "project_id", :string, :limit => 30, :default => "", :null => false
+    create_table :pic_ups, :force => true do |t|
+      t.column "project_id", :string, :limit => 30, :null => false
       t.column "activated", :boolean, :default => false, :null => false
       t.column "day",:integer, :default => 0, :null => false
       t.column "num_day",:integer, :default => 0, :null => false
